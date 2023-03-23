2b378f5ff097e93b96f7149668170e2f16d5f724
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-03-22 21:48:53 -0300

    Create exemplo.rb

---
+++ Linguagem Ruby/exemplo.rb
@@ -0,0 +1,14 @@
+#Aqui está um exemplo simples de código em Ruby que imprime "Hello, World!" na tela. Aqui está um passo a passo para escrever e executar este código:
+
+#1-Abra um editor de texto, como o Bloco de Notas no Windows ou o TextEdit no Mac.
+#2-Escreva o seguinte código em Ruby no editor de texto:
+
+puts "Hello, World!"
+
+#Este código usa a função puts para imprimir "Hello, World!" na tela.
+
+#1-Salve o arquivo com o nome "hello.rb" e a extensão .rb (por exemplo, "hello.rb").
+#2-Abra o terminal do sistema operacional.
+#3-Navegue até o diretório onde o arquivo "hello.rb" está localizado usando o comando cd.
+#4-Digite o comando ruby hello.rb para executar o programa.
+#5-O resultado "Hello, World!" será impresso na tela.
