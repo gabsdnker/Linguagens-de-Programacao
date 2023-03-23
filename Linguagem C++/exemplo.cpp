623762165fd81c89355b5a614b51feae564a0510
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-03-22 21:24:32 -0300

    Create exemplo.cpp

---
+++ Linguagem C++/exemplo.cpp
@@ -0,0 +1,25 @@
+// 1-Abra um editor de texto, como o Bloco de Notas no Windows ou o TextEdit no Mac.
+// 2-Escreva o seguinte código em C++ no editor de texto:
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+    return 0;
+}
+
+//Este código usa a biblioteca iostream para acessar as funções cout e endl, que são usadas para imprimir texto na tela. 
+//A função main() é o ponto de entrada do programa.
+
+//1-Salve o arquivo com o nome "hello.cpp" e a extensão .cpp (por exemplo, "hello.cpp").
+//2-Abra um terminal ou linha de comando e navegue até o diretório onde você salvou o arquivo "hello.cpp".
+//3-Compile o código-fonte usando o compilador C++ digitando o seguinte comando:
+
+//g++ hello.cpp -o hello
+
+//Este comando usa o compilador C++ (g++) para compilar o arquivo "hello.cpp" e criar um arquivo executável chamado "hello".
+
+//1- Execute o programa digitando o seguinte comando:
+//  ./hello
+
+//Este comando executa o arquivo executável "hello", que imprime "Hello, World!" na tela.
