7607f8c7444fb2bfba7fd76be56b1b5762b30ae6
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-03-22 21:35:44 -0300

    Update exemplo.java

--- Linguagem Java/exemplo.java
+++ Linguagem Java/exemplo.java
@@ -1,1 +1,21 @@
+//Aqui está um exemplo simples de código em Java que imprime "Hello, World!" na tela. Aqui está um passo a passo para escrever e executar este código:
 
+//1-Abra um editor de texto, como o Bloco de Notas no Windows ou o TextEdit no Mac.
+//2-Escreva o seguinte código em Java no editor de texto:
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+//Este código define uma classe chamada HelloWorld com um método main() que imprime "Hello, World!" na tela.
+
+//1-Salve o arquivo com o nome "HelloWorld.java" e a extensão .java (por exemplo, "HelloWorld.java").
+//2-Abra um terminal ou linha de comando e navegue até o diretório onde você salvou o arquivo "HelloWorld.java".
+//3-Compile o código-fonte usando o compilador Java digitando o seguinte comando:
+
+//     javac HelloWorld.java
+
+//Este comando usa o compilador Java (javac) para compilar o arquivo "HelloWorld.java" e criar um arquivo de classe chamado "HelloWorld.class".
+//1- Execute o programa.
