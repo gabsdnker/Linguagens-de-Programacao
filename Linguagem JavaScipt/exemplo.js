5ab74ada32d784ecedc91761a6654545670998a3
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2023-03-22 21:43:08 -0300

    Create exemplo.js

---
+++ Linguagem JavaScipt/exemplo.js
@@ -0,0 +1,16 @@
+<!-- Aqui está um exemplo simples de código em JavaScript que imprime "Hello, World!" na tela. Aqui está um passo a passo para escrever e executar
+<!--este código: 
+
+<!-- 1-Abra um editor de texto, como o Bloco de Notas no Windows ou o TextEdit no Mac.
+<!-- 2-Escreva o seguinte código em JavaScript no editor de texto:
+
+console.log("Hello, World!");
+
+<!-- Este código usa a função console.log() para imprimir "Hello, World!" no console do navegador.
+
+<!-- 1-Salve o arquivo com o nome "hello.js" e a extensão .js (por exemplo, "hello.js").
+<!-- 2-Abra um navegador web, como o Google Chrome ou o Mozilla Firefox.
+<!-- 3-Abra a ferramenta de desenvolvedor do navegador pressionando F12 no teclado.
+<!-- 4-Navegue até a guia Console na ferramenta de desenvolvedor.
+<!-- 5-Carregue a página HTML que contém o código JavaScript (ou adicione o código diretamente na página HTML).
+<!-- 6-O resultado "Hello, World!" será impresso no console do navegador.
